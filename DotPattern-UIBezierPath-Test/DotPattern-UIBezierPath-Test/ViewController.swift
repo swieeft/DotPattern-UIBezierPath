2ab1cce7b59b114732ae82a05f24b28ab8d0fd8b
박길남 <gilnam_park@genieworks.net>
2018-08-31 10:17:47 +0900

    1. 최초 프로젝트 등록

---
+++ DotPattern-UIBezierPath-Test/DotPattern-UIBezierPath-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DotPattern-UIBezierPath-Test
+//
+//  Created by 박길남 on 2018. 8. 31..
+//  Copyright © 2018년 swieeft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DotPattern-UIBezierPath/DotPattern-UIBezierPath/DotPattern_UIBezierPath.h
@@ -0,0 +1,19 @@
+//
+//  DotPattern_UIBezierPath.h
+//  DotPattern-UIBezierPath
+//
+//  Created by 박길남 on 2018. 8. 31..
+//  Copyright © 2018년 swieeft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DotPattern_UIBezierPath.
+FOUNDATION_EXPORT double DotPattern_UIBezierPathVersionNumber;
+
+//! Project version string for DotPattern_UIBezierPath.
+FOUNDATION_EXPORT const unsigned char DotPattern_UIBezierPathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DotPattern_UIBezierPath/PublicHeader.h>
+
+
