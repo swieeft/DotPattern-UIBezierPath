7bf974271ae3e0cd388d27ce36a35cfd2cceba8f
박길남 <gilnam_park@genieworks.net>
2018-08-31 10:59:58 +0900

    1. DotPattern 추가

---
+++ DotPattern-UIBezierPath/DotPattern-UIBezierPath/DotPattern.swift
@@ -0,0 +1,130 @@
+//
+//  DotPattern.swift
+//  DotPattern-UIBezierPath
+//
+//  Created by 박길남 on 2018. 8. 31..
+//  Copyright © 2018년 swieeft. All rights reserved.
+//
+import Foundation
+import UIKit
+
+struct Dot: Hashable {
+    var row: Int
+    var col: Int
+    
+    init(row: Int, col: Int) {
+        self.row = row
+        self.col = col
+    }
+}
+
+class DotPattern {
+    private var dotPoints:[Dot:CGPoint] = [:]
+    
+    func setDot(view:UIView, row:Int, col:Int) {
+        
+        let rowSize = view.frame.height / CGFloat(row)
+        let colSize = view.frame.width / CGFloat(col)
+        
+        for i in 1...row {
+            let rowPointY = rowSize * CGFloat(i)
+            let colPointY = rowSize * CGFloat(i - 1)
+            
+            for j in 1...col {
+                let rowPointX = colSize * CGFloat(j - 1)
+                let colPointX = colSize * CGFloat(j)
+                
+                let centerX = (rowPointX + colPointX) / 2
+                let centerY = (rowPointY + colPointY) / 2
+                
+                let center = CGPoint(x: centerX, y: centerY)
+                
+                let key = Dot(row: i, col: j)
+                dotPoints[key] = center
+            }
+        }
+    }
+    
+    func viewDot(view:UIView, layer:CAShapeLayer)  {
+        let path = UIBezierPath()
+        
+        for point in dotPoints {
+            path.move(to: point.value)
+            path.addLine(to: CGPoint(x: point.value.x, y: point.value.y + 2))
+        }
+        
+        view.layer.addSublayer(layer)
+    }
+    
+    func viewDot(view:UIView, layer:CAShapeLayer, rowRange: CountableClosedRange<Int>, colRange: CountableClosedRange<Int>)  {
+        let path = UIBezierPath()
+        
+        for i in rowRange {
+            for j in colRange {
+                let key = Dot(row: i, col: j)
+                let point = dotPoints[key]
+                
+                if point != nil {
+                    path.move(to: point!)
+                    path.addLine(to: CGPoint(x: point!.x, y: point!.y + 2))
+                }
+            }
+        }
+        
+        view.layer.addSublayer(layer)
+    }
+    
+    func isEmpty(row: Int, col: Int) -> Bool {
+        let key = Dot(row: row, col: col)
+        
+        if let _ = dotPoints[key] {
+            return false
+        }
+        
+        return true
+    }
+    
+    func get(_ row: Int, _ col: Int) -> CGPoint {
+        let key = Dot(row: row, col: col)
+        
+        if let point = dotPoints[key] {
+            return point
+        }
+        
+        return CGPoint(x: 0, y: 0)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
